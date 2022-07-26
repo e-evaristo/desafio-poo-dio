3a7ba73879c3d7b37949ceb380ffd6d9e8bd22a1
e-evaristo <emanoelevaristo@hotmail.com>
2022-07-26 20:08:26 -0300

    Create Abstract class Conteudo, change Curso & Mentoria heritage

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+        Curso curseA = new Curso();
+        curseA.setTitulo("Curso JAVA");
+        curseA.setDescricao("Descrição do Curso JAVA");
+        curseA.setCargaHoraria(8);
+
+        Curso curseB = new Curso();
+        curseB.setTitulo("Curso JavaScript");
+        curseB.setDescricao("Descrição do Curso JavaSript");
+        curseB.setCargaHoraria(4);
+
+        Mentoria mentorA = new Mentoria();
+        mentorA.setTitulo("Mentoria Java");
+        mentorA.setDescricao("Descrição da Mentoria JAVA");
+        mentorA.setData(LocalDate.now());
+
+        System.out.println(curseA);
+        System.out.println(curseB);
+        System.out.println(mentorA);
 
     }
 }
